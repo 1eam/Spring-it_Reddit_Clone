4e1b232a094f3003a41912ffeaf831818bd21c17
Esther <esther191299@gmail.com>
2019-11-19 01:45:26 +0100

    Starter classes: Link, Comment, Vote set up. Old way of setting up Entity-Classes

--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -1,40 +1,13 @@
 package com.vega.springit;
 
-import com.vega.springit.config.SpringitProperties;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-
-import java.sql.SQLOutput;
 
 @SpringBootApplication
-@EnableConfigurationProperties(SpringitProperties.class)
+
 public class SpringitApplication {
-
-
-    @Autowired
-    private SpringitProperties springitProperties;
-
     public static void main(String[] args) {
         SpringApplication.run(SpringitApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner runner() {
-        return args -> {
-            System.out.println("Welcome message: " + springitProperties.getWelcomeMsg());
-        };
-    }
-
-    @Bean
-    @Profile("dev")
-    CommandLineRunner runner2() {
-        return args -> {
-            System.out.println("Dir rund alleen op de profile: 'dev'");
-        };
-    }
 }
