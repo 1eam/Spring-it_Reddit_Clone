47e75742f370c5f5e1bd8fff7873c801de147952
Esther <esther191299@gmail.com>
2019-11-19 23:54:17 +0100

    Added mapping: link -> comment

--- src/main/java/com/vega/springit/domain/Link.java
+++ src/main/java/com/vega/springit/domain/Link.java
@@ -7,6 +7,9 @@ import lombok.NonNull;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -22,4 +25,7 @@ public class Link {
     @NonNull
     private String url;
 
+    //Link has many comments
+    @OneToMany(mappedBy = "link")
+    private List<Comment> Comments = new ArrayList<>();
 }
