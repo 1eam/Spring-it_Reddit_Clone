89652d1e2f2882dcbe02521deb325bb4ce59b5a4
Esther <esther191299@gmail.com>
2019-12-03 06:09:23 +0100

    Added roles, repos and a Userdetails service

--- src/main/java/com/vega/springit/domain/User.java
+++ src/main/java/com/vega/springit/domain/User.java
@@ -2,12 +2,14 @@ package com.vega.springit.domain;
 
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Size;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Entity
 @RequiredArgsConstructor
@@ -15,7 +17,7 @@ import javax.validation.constraints.Size;
 @Setter
 @ToString
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id @GeneratedValue
     private Long id;
@@ -33,5 +35,41 @@ public class User {
     @Column(nullable = false)
     private boolean enabled;
 
+/*Relationship*/
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+
+/*Methods overriding*/
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 }
