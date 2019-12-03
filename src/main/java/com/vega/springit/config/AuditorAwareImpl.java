18040a27f8fe2e44dc00ad7cf5d851f9d7eab235
Esther <esther191299@gmail.com>
2019-12-03 07:50:19 +0100

    Added Auditing (created by + creationtime to Users

---
+++ src/main/java/com/vega/springit/config/AuditorAwareImpl.java
@@ -0,0 +1,25 @@
+package com.vega.springit.config;
+
+import com.vega.springit.domain.User;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditorAwareImpl implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        //      Link class cant get logged in user (cause Link extends Auditable) to fix this (see code below) (this is only a development-only  issue.
+        if(SecurityContextHolder.getContext().getAuthentication() == null){
+            return Optional.of("master@gmail.com");
+        }
+
+//        normal code
+            return Optional.of(((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getEmail());
+
+    }
+}
